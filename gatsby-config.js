1b19180b7f27bfef16509f7d986d4c615ee64cab
Ondřej Chrastina <chrastina.ondra@gmail.com>
2020-06-12 13:37:06 +0200

    connetct to sample project of the Kontent

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,16 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: '@kentico/gatsby-source-kontent',
+      options: {
+        projectId: '975bf280-fd91-488c-994c-2f04416e5ee3', // Fill in your Project ID
+        languageCodenames: [
+          'en-US',
+          'es-ES', // Languages in your project (Project settings -> Localization),
+        ],
+      },
+    },
+  ],
 }
